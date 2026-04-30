4ac86d11983a3829fe8051673e12c33b29143f25
josvan-edu <josvangonzalez@hotmail.com>
2026-04-30 14:52:39 -0600

    I hate nerdfonts

--- lua/plugin_configs/alphavim.lua
+++ lua/plugin_configs/alphavim.lua
@@ -4,9 +4,8 @@ local dashboard = require("alpha.themes.dashboard")
 
 local function footer()
   local v = vim.version()
-  local datetime = os.date " %d-%m-%Y   %H:%M:%S"
-  local platform = vim.fn.has "win32" == 1 and "" or ""
-  return string.format(" v%d.%d.%d %s  %s", v.major, v.minor, v.patch, platform, datetime)
+  local datetime = os.date "%d-%m-%Y  %H:%M:%S"
+  return string.format("v%d.%d.%d %s  %s", v.major, v.minor, v.patch, platform, datetime)
 end
 
 local banners = {
@@ -34,9 +33,9 @@ dashboard.section.header.val = banners['punoko']
 --dashboard.section.header.opts.hl = "AlphaCol" .. math.random(5)
 
 dashboard.section.buttons.val = {
-  dashboard.button("f", "  > Find file", ":cd $HOME| Telescope find_files<CR>"),
-  dashboard.button("r", "  > Recent files" , ":Telescope oldfiles<CR>"),
-  dashboard.button("s", "  > NVIM Settings" , ":e $MYVIMRC | :cd %:p:h | wincmd k | pwd<CR>"),
+  dashboard.button("f", "> Find file", ":cd $HOME| Telescope find_files<CR>"),
+  dashboard.button("r", "> Recent files" , ":Telescope oldfiles<CR>"),
+  dashboard.button("s", "> NVIM Settings" , ":e $MYVIMRC | :cd %:p:h | wincmd k | pwd<CR>"),
 }
 
 dashboard.section.footer.val = footer()
